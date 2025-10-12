7113543f98ea1775c7655585d943133798fdaf4c
Kopite22 <nulee1000@gmail.com>
2025-10-12 16:21:59 +0900

    ✨ - 카카오로그인 res 추가

--- src/shared/api/queryFactory/auth/types.ts
+++ src/shared/api/queryFactory/auth/types.ts
@@ -1,3 +1,8 @@
 export interface KakaoParams {
   idToken: string;
 }
+
+export interface KakaoResponse {
+  accessToken: string;
+  isNewUser: boolean;
+}
--- src/views/login/AuthPage.tsx
+++ src/views/login/AuthPage.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 // import { useMutation, useQuery } from "@tanstack/react-query";
-
+// import { AxiosError } from "axios";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
 // import { authQueries } from "@/shared/api/queryFactory/auth";
+// import {
+//   KakaoParams,
+//   KakaoResponse,
+// } from "@/shared/api/queryFactory/auth/types";
 import { ROUTE_PATH } from "@/shared/constants";
 import { Loader } from "@/shared/ui/loader";
+
 // import { PortalOverlay } from "@/shared/ui/portalOverlay";
 
 const AuthPage = () => {
@@ -15,10 +20,14 @@ const AuthPage = () => {
   const router = useRouter();
   const idToken = queryString.get("code");
 
-  // const { mutate } = useMutation(
+  // const { mutate } = useMutation<KakaoResponse, AxiosError, KakaoParams>(
   //   authQueries.postKakao({
   //     onSuccess: (data) => {
-  //       console.log(data);
+  //       if (data.isNewUser) {
+  //         router.push(ROUTE_PATH.SIGN_UP);
+  //       } else {
+  //         router.push(ROUTE_PATH.HOME);
+  //       }
   //     },
   //     onError: (error) => {
   //       console.log(error);
